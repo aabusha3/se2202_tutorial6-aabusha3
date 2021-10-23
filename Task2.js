832fa1fc3ff1954189a966b876800364191fba1b
Ahmad <aabusha3@uwo.ca>
2021-10-23 00:44:11 -0400

    Complex Number Add and Subtract functions

--- Task2.js
+++ Task2.js
@@ -1,46 +1,43 @@
-function ComplexNumber(real, imaginary)
-{
+function ComplexNumber(real, imaginary) {
     /*
         Define and initialize the properties representing the real and imaginary components
         and the print function as it was defined in task 1
     */
-      
-    this.print = function()
-        {         
-            // Please replace the comments with the numberic values only, don't change the printed string because it'll be used for testing
-            console.log(/*refer to the real part*/+" , " + /*refer to the imagenary part*/+ "i");
-        }
+    this.real = real;
+    this.imaginary = imaginary;
 
-    
+    this.print = function() {
+        // Please replace the comments with the numberic values only, don't change the printed string because it'll be used for testing
+        console.log(real + " + " + imaginary + "i");
+    }
 
-    this.add = function(theOtherComplexNumber)
-    {
+
+
+    this.add = function(theOtherComplexNumber) {
         /**
           Complete this function so that it would receive an object of a complex number as an argument.
           Since it’s defined as a part of a complex number object itself, it has access to its own real and complex components (through the ‘this’ keyword).
            Create and return a new object that will contain the result of adding the received object to the current object. 
          */
-       
+        return new ComplexNumber(theOtherComplexNumber.real + real, theOtherComplexNumber.imaginary + imaginary);
     };
 
-    this.subtract = function(theOtherComplexNumber)
-    {
+    this.subtract = function(theOtherComplexNumber) {
         /*
             Similar to the add method but the subtract operatin applied
         */
-       
+        return new ComplexNumber(real - theOtherComplexNumber.real, imaginary - theOtherComplexNumber.imaginary);
     };
 
     // the multiplication and the division are a little more involved, so they're not required
     // feel free to define them if you're interested; you won't be penalized if you don't
 };
 
-let c1; // use the constructor function to create the complex number 2 + 5i
-let c2; // use the constructor function to create the complex number 4 + 10i
+let c1 = new ComplexNumber(2, 5); // use the constructor function to create the complex number 2 + 5i
+let c2 = new ComplexNumber(4, 10); // use the constructor function to create the complex number 4 + 10i
 
 let c3 = c1.add(c2);
 c3.print();
 
-let c4= c1.subtract(c2);
+let c4 = c1.subtract(c2);
 c4.print();
-
